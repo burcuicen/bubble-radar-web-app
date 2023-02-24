582b056c81a8f86a06cffe7b70098a3533987053
burcuicen <burcuicen2000@gmail.com>
2023-02-25 01:14:02 +0300

    remove useless codes

--- src/App.vue
+++ src/App.vue
@@ -7,36 +7,5 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  created() {
-    this.getTrending();
-    this.getPopularSearches();
-  },
-  methods: {
-    async getTrending() {
-      const { err, res } = await this.$api.trending.getAll();
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(res);
-      }
-
-    },
-    async createKeywords(letter: string) {
-      const { err, res } = await this.$api.trending.createByAlphabet(letter);
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(res);
-      }
-    },
-    async getPopularSearches() {
-      const { err, res } = await this.$api.popular.getAll();
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(res);
-      }
-    }
-  }
 })
 </script>
