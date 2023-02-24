35287ccb0d69863cb4c935e29271625fc990ff2f
burcuicen <burcuicen2000@gmail.com>
2023-02-25 01:25:17 +0300

    configure pug

--- quasar.config.js
+++ quasar.config.js
@@ -52,6 +52,12 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
+      extendWebpack(cfg) {
+        cfg.module.rules.push({
+          test: /\.pug$/,
+          loader: 'pug-plain-loader',
+        });
+      },
 
       // transpile: false,
       // publicPath: '/',
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,5 @@
-<template>
-  <q-page class="row items-center justify-evenly">
-  </q-page>
+<template lang="pug">
+q-page.row.items-center fdghj
 </template>
 
 <script lang="ts">
