8b6ef7ca229cfebe06d83a56b7cc2e30a03b4322
burcuicen <burcuicen2000@gmail.com>
2023-05-04 21:43:02 +0300

    feat: fix trademark checker styles

--- src/pages/trademark-checker.vue
+++ src/pages/trademark-checker.vue
@@ -2,20 +2,23 @@
 .row.q-col-gutter-lg
   .col-12
     .text-h6.q-mb-md Trademark Checker
+    .text-subtitle.q-mb-md.text-grey-8 Looking to check multiple trademarks? Our trademark checker allows you to enter multiple entries, separated by a newline, and quickly search a database of registered trademarks to determine their availability. Avoid potential legal issues and ensure your brand or product names are available for registration with our easy-to-use trademark checker. Try it out now!
     q-input(v-model="trademarkList" filled type="textarea" label="Trademark List" hint="Enter your trademark list here")
     q-btn(@click="generate" color="primary" label="Search" class="q-mt-md")
   .col-12(v-if="results.length")
     .text-h6.q-mt-md Results
     .text-subtitle.q-mb-md {{ results.length }} results found
-    .q-pa-md
-      q-list
-        q-item(v-for="item in results" clickable)
-          q-item-section
-            q-item-label {{ item.trademarkName }}
-            q-item-label(caption lines=2) Showing results for the search term: {{ item.query }}
-          q-item-section(side top)
-            q-badge(outline :label="item.trademarkStatus" :color="getColor(item.trademarkStatus)")
-          q-separator
+    q-list(bordered separator)
+      q-item(v-for="item in results" clickable style="padding:20px")
+        q-badge(floating :color="getColor(item.trademarkStatus)" rounded)
+        q-icon.q-mr-md(:name="getIcon(item.trademarkName)" size="2em" :color="getIconColor(item.trademarkName)")
+        q-item-section
+          q-item-label {{ item.trademarkName }}
+          q-item-label(caption lines=2) Showing results for the search term:
+            b(:style="getStyle(item.trademarkStatus)")  {{ item.query }}
+        q-item-section(side top)
+          q-badge(outline :label="item.trademarkStatus" :color="getColor(item.trademarkStatus)")
+        q-separator
 </template>
 <script>
 import { defineComponent } from 'vue';
@@ -43,11 +46,23 @@ export default defineComponent({
       console.log(this.results, this.columns)
 
     },
+    getIconColor(name) {
+      if (name.split(' ').length > 1) return 'red';
+      return 'yellow';
+    },
+    getIcon(name) {
+      if (name.split(' ').length > 1) return 'warning';
+      return 'radio_button_checked';
+    },
     getColor(status) {
       if (status === 'LIVE') return 'green';
       if (status === 'DEAD') return 'red';
       return 'grey';
     },
+    getStyle(status) {
+      const color = this.getColor(status);
+      return `color:${color};`;
+    }
   },
 });
 </script>
